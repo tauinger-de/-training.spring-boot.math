ade4da4ceaea71a1dc758912ffc0c99805dd8596
Thomas Auinger <thomas@auinger.de>
2022-10-27 20:50:22 +0200

    starting with 020 solution

---
+++ src/test/java/de/auinger/training/springboot/math/MathApplicationTests.java
@@ -0,0 +1,13 @@
+package de.auinger.training.springboot.math;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
