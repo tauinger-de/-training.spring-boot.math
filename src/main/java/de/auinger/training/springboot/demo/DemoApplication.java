dd5d8296861f5752da04531e46e1e203c3d4e96e
Thomas Auinger <thomas@auinger.de>
2022-10-28 10:05:40 +0200

    back to DemoApplication and WelcomingRunner, taken from spring-adv

---
+++ src/main/java/de/auinger/training/springboot/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package de.auinger.training.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Los geht's!");
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
