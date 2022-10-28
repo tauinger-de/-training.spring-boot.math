40ad9c9c820263faae0c6edae35d986d02db586c
Thomas Auinger <thomas@auinger.de>
2022-10-28 09:02:08 +0200

    changes for 000_start

--- src/main/java/de/auinger/training/springboot/math/MathApplication.java
+++ src/main/java/de/auinger/training/springboot/math/MathApplication.java
@@ -3,11 +3,18 @@ package de.auinger.training.springboot.math;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class MathApplication {
 
     public static void main(String[] args) {
+        System.out.println("Starting Spring Boot application. Hold tight...");
         SpringApplication.run(MathApplication.class, args);
     }
 
+    @PostConstruct
+    public void sayHello() {
+        System.out.println("Application started -- hello everybody!");
+    }
 }
