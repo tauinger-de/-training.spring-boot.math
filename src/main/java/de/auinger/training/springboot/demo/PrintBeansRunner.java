72a266c6e2b3bef7edbf4ad24c56bcb4a808aeef
Thomas Auinger <thomas@auinger.de>
2023-01-30 11:57:31 +0100

    added PrintBeansRunner

---
+++ src/main/java/de/auinger/training/springboot/demo/PrintBeansRunner.java
@@ -0,0 +1,56 @@
+package de.auinger.training.springboot.demo;
+
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import static java.lang.System.out;
+
+@Component
+public class PrintBeansRunner implements CommandLineRunner {
+
+    private final ListableBeanFactory factory;
+    private final String applicationPackagePrefix;
+
+    @Autowired
+    public PrintBeansRunner(ListableBeanFactory listableBeanFactory) {
+        this(listableBeanFactory, "de.auinger.training.springboot");
+    }
+
+    public PrintBeansRunner(ListableBeanFactory listableBeanFactory, String applicationPackagePrefix) {
+        this.factory = listableBeanFactory;
+        this.applicationPackagePrefix = applicationPackagePrefix;
+    }
+
+    @Override
+    public void run(String... args) {
+        var allBeansById = factory.getBeansOfType(Object.class);
+        if (applicationPackagePrefix == null || applicationPackagePrefix.isBlank()) {
+            printBeans(allBeansById, "bean(s)", (obj) -> true);
+        } else {
+            Predicate<Object> applicationClassPredicate =
+                    (bean) -> bean.getClass().getPackageName().startsWith(applicationPackagePrefix);
+            printBeans(allBeansById, "Spring bean(s)", Predicate.not(applicationClassPredicate));
+            printBeans(allBeansById, "application bean(s)", applicationClassPredicate);
+        }
+    }
+
+    private void printBeans(Map<String, Object> beansById, String beanOrigin, final Predicate<Object> beanPredicate) {
+        var sortedBeanNames = beansById.entrySet().stream()
+                .filter(entry -> beanPredicate.test(entry.getValue()))
+                .map(entry -> entry.getKey())
+                .sorted()
+                .collect(Collectors.toList());
+
+        out.printf("The context contains %d %s in total:\n", sortedBeanNames.size(), beanOrigin);
+        for (String name : sortedBeanNames) {
+            out.printf("  - %s : \u001B[36m%s\u001B[0m\n", name, factory.getType(name).getName());
+        }
+    }
+
+}
